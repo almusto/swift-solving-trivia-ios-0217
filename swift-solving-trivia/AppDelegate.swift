eface36fac79a0dc456c77332d5faca9b39bb25d
almusto <ales.musto@gmail.com>
2017-01-31 13:46:20 -0500

    solved

--- swift-solving-trivia/AppDelegate.swift
+++ swift-solving-trivia/AppDelegate.swift
@@ -14,16 +14,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     
-    func solveTrivia(_ trivia: [String : String]) -> String {
-        
-        // Implement this function!
-        
-        return "Hi :D"
-        
+  func solveTrivia(_ trivia: [String : String]) -> String {
+    var badArray : [Character] = []
+    for (state, capital) in trivia {
+      let stateChars = Array(state.lowercased().characters)
+      let capitalChars = Array(capital.lowercased().characters)
+
+      for char in stateChars {
+        if capitalChars.contains(char) {
+          badArray.append(char)
+        }
+      }
+      if badArray.count == 0 {
+        return state
+      } else {
+        badArray = []
+      }
+
     }
-    
-    
-    
+    return "Hi :D"
+  }
     
 }
 
